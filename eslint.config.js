bab727e18f8ac03b95b7ed8fec1b0f3958b6853c
Maksym Kovpak <kovpak.maksym@gmail.com>
2025-07-15 12:13:45 +0300

    Create basic Vite configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import tsEslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
+export default tsEslint.config([
+  globalIgnores(["es", "lib"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tsEslint.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite
     ],
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,54 @@
-import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react-swc";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import { peerDependencies } from "./package.json";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      outDir: ["es", "lib"],
+      tsconfigPath: resolve(__dirname, "tsconfig.app.json")
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: "always",
+        relativeUrls: true,
+        javascriptEnabled: true
+      }
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: format => (format === "es" ? "index.js" : "index.cjs")
+    },
+    rollupOptions: {
+      output: [
+        {
+          exports: "named",
+          format: "es",
+          dir: "es",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].js"
+        },
+        {
+          exports: "named",
+          format: "cjs",
+          dir: "lib",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].cjs"
+        }
+      ],
+      external: ["react/jsx-runtime", ...Object.keys(peerDependencies || {})]
+    },
+    sourcemap: true
+  }
 });
