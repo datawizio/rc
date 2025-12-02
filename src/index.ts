4b391503f0e764ee3596e815ce639417b17f7cd2
Maksym Kovpak <kovpak.maksym@gmail.com>
2025-12-02 10:49:20 +0200

    Update dependencies to the latest versions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// TODO: Remove this line when AntDesign becomes compatible with React 19
-import "@ant-design/v5-patch-for-react-19";
-
 export { default as ApiError } from "./components/ApiError";
 export { default as AppFooter } from "./components/AppFooter";
 export { default as AppLoader } from "./components/AppLoader";
