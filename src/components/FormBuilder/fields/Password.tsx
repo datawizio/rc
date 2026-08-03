b75a12c214ee56421c2a5939a4b52fda617f4844
Maksym Kovpak <kovpak.maksym@gmail.com>
2026-08-03 18:05:01 +0300

    Refactor `FieldPassword` component to accept additional form item properties

--- src/components/FormBuilder/fields/Password.tsx
+++ src/components/FormBuilder/fields/Password.tsx
@@ -1,16 +1,21 @@
 import React, { useCallback } from "react";
 import { Form, Input } from "antd";
 
+import type { FormItemProps } from "antd";
 import type { FieldTextProps } from "../types";
 
-export const FieldPassword: React.FC<FieldTextProps> = React.memo(
-  ({ label, rules, name, placeholder, onChange, ...props }) => {
+export interface FieldPasswordProps extends FieldTextProps {
+  formItemProps?: FormItemProps;
+}
+
+export const FieldPassword: React.FC<FieldPasswordProps> = React.memo(
+  ({ label, rules, name, placeholder, onChange, formItemProps, ...props }) => {
     const handleFieldChange = useCallback<
       React.ChangeEventHandler<HTMLInputElement>
     >(({ target: { value } }) => onChange?.({ name, value }), [name, onChange]);
 
     return (
-      <Form.Item name={name} label={label} rules={rules}>
+      <Form.Item name={name} label={label} rules={rules} {...formItemProps}>
         <Input.Password
           {...props}
           placeholder={placeholder}
